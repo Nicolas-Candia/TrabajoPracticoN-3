a1acfb0e19f5b5434d493ccfbe346e8ddcb5f7c2
Nicolas-Candia <nicolascandia689@gmail.com>
2026-05-29 01:47:01 -0300

    agrego una tabla de referencias para comparar el resultado

--- bmi.c
+++ bmi.c
@@ -13,7 +13,14 @@ int main(){
    imc = (float) (peso+altura/100)/2;
 
    printf("su indice de masa corporal es: %3.2f\n",imc);
-
+   
+   printf("tabla de referencia:\n");
+   printf("índice     \t|\tcondicion\n");
+   printf("-------------------------\n");
+   printf("<18.5      \t|\tbajo peso\n");
+   printf("18.5 a 24.9\t|\tnormal\n");
+   printf("25 a 29.9\t|\tsobrepeso\n");
+   printf(">30        \t|\tobesidad\n");
    return 0;
 
 	
