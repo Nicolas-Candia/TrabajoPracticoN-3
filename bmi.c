0abb8adec6dc3d595c96d13fe24099f863fe81c0
Nicolás-Candia <nicolascandia689@gmail.com>
2026-05-29 00:14:55 -0300

    Calculadora del IMC

---
+++ bmi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+
+   int peso;
+   int altura;
+   float imc;
+
+   printf("ingrese su peso en Kg:");
+   scanf("%d",&peso);
+   printf("ingrese su altura en Centimetros:");
+   scanf("%d",&altura);
+
+   imc = (float) (peso+altura)/2;
+
+   printf("su indice de masa corporal es: %3.2f\n",imc);
+
+   return 0;
+
+	
+}
