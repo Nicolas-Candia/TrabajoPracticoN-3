73d4ae045def25d926943e4ff2cd2fcc6f7495a3
Nicolas-Candia <nicolascandia689@gmail.com>
2026-07-21 02:06:53 -0300

    Ahora ell programa indicara el estado del usuario.

--- bmi.c
+++ bmi.c
@@ -23,6 +23,15 @@ int main(){
    printf("18.5 a 24.9\t|\tnormal\n");
    printf("25 a 29.9\t|\tsobrepeso\n");
    printf(">30        \t|\tobesidad\n");
-
+   
+   if (imc >= 30){
+	   printf("Su estado es: \"obesidad\".\n");
+   } else if (imc >= 25){
+	   printf("Su estado es: \"sobrepeso\".\n");
+   } else if (imc >= 18.5){
+	   printf("Su estado es: \"normal\"\n.");
+   } else {
+	   printf("Su estado es: \"bajo peso\".\n");
+   }
    return 0;
 }
